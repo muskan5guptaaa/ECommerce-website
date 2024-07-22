d1cdf047d9a695391b742390e7a79047bcaf1d09
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-07-22 12:57:23 +0530

    initial commit

--- seed.js
+++ seed.js
@@ -1,33 +1,45 @@
-const mongoose = require('mongoose'); //object
+const mongoose = require('mongoose');
+
 const Product = require('./models/Product');
 
 
-
 const products = [
-    { 
-        name:"iphone 15pro" , 
-        img: "https://images.unsplash.com/photo-1695048133142-1a20484d2569?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aXBob25lJTIwMTV8ZW58MHx8MHx8fDA%3D" , 
-        price: 124000,
-        desc: "very costly phone it is"
+    {
+        name:"Iphone 14pro",
+        img:"https://images.unsplash.com/photo-1663499482523-1c0c1bae4ce1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGlwaG9uZTE0cHJvfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" ,
+        price: 130000,
+        desc: "very costly, aukaat ke bahar" 
     },
-    { 
-        name: "macbook pro", 
-        img: "https://images.unsplash.com/photo-1598495037740-2c360cf49e50?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bWFjJTIwYm9vayUyMHByb3xlbnwwfHwwfHx8MA%3D%3D" , 
-        price: 230000,
-        desc: "hello i am a good machine"
+    {
+        name:"Macbook m2 pro",
+        img:"https://media.istockphoto.com/id/1441194641/photo/macbook-pro-16-keyboard.webp?b=1&s=170667a&w=0&k=20&c=vgawd7HFjNtU4H4596IZkU2SE9YRXtz-ROUgSvndAPQ=",
+        price: 250000 , 
+        desc:"ye to bilkul hi aukaatke bahar"
     },
-    { 
-        name: "apple pencil", 
-        img: "https://images.unsplash.com/photo-1551651639-927b595f815c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBwZW5jaWx8ZW58MHx8MHx8fDA%3D", 
-        price: 10000,
-        desc: "i can write future"
+    {
+        name:"Iwatch",
+        price:51000,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROzEBWSIT--95okkMi-UzyJcT0RpNufBUVCw&usqp=CAU",
+        desc:"ye sasta hai lelo"
+    },
+    {
+        name:"iPad Pro", 
+        img: "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aXBhZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60", 
+        price: 237900, 
+        desc: "life mein kuch cheese sirf dekhne ke liye hota hai"
+    },
+    {
+        name:"airpods" , 
+        img:"https://images.unsplash.com/photo-1606841837239-c5a1a4a07af7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFpcnBvZHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" , 
+        price: 25000 ,
+        desc: "badiya hai kamao kamao"
     }
 ]
 
-
 async function seedDB(){
+    // await Product.deleteMany({});
     await Product.insertMany(products);
-    console.log("DB seeded")
+    console.log("data seeded successfully");
 }
 
 module.exports = seedDB;
