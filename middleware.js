113729c599021a7017f546ff8bc00cc78205ff53
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-08-10 14:08:00 +0530

    initial commmit

--- middleware.js
+++ middleware.js
@@ -1,55 +1,71 @@
+const { productSchema } = require("./schema");
+const { reviewSchema } = require("./schema");
+const passport = require('passport');
+const Product = require("./models/Product");
 
-const Product = require('./models/Product');
-const {productSchema , reviewSchema} = require('./schema')
+
+const isLoggedIn = (req,res,next)=>{
+    // console.log(req.originalUrl);
+    // console.log(req.xhr);
+    if(req.xhr && !req.isAuthenticated()){
+        return res.status(401).json({msg:'you need to login first'});
+    }
+    
+    if(!req.isAuthenticated()){
+        req.flash('error' , 'you need to login first');
+        return res.redirect('/login');
+    }
+    next();
+} 
+
 
 const validateProduct = (req,res,next)=>{
-    const {name,img,price,desc} = req.body;
-    const {error} = productSchema.validate({name,img,price,desc})
+    const {name, img, price , desc} = req.body;
+    const {error} = productSchema.validate({name,img,price,desc});
+    
     if(error){
-        return res.render('error');
+        const msg = error.details.map((err)=>err.message).join(',');
+        return res.render('error' , {err:msg});
     }
     next();
 }
 
 const validateReview = (req,res,next)=>{
-    const {rating,comment} = req.body;
-    const {error} = reviewSchema.validate({rating,comment})
+
+    const {rating, comment} = req.body;
+    const {error} = reviewSchema.validate({rating,comment});
+
     if(error){
-        return res.render('error');
+        const msg = error.details.map((err)=>err.message).join(',');
+        return res.render('error' , {err:msg});
     }
     next();
 }
-const isLoggedIn = (req,res,next)=>{
-    if(!req.isAuthenticated()){
-        req.flash('error' , 'please login first');
-        return res.redirect('/login');
-    }
-    next();
-}
+
 const isSeller = (req,res,next)=>{
     if(!req.user.role){
-        req.flash('error' , 'You donot have the permission to do that');
+        req.flash('error' , 'you donot have the permission to do that');
         return res.redirect('/products');
     }
     else if(req.user.role !== 'seller'){
-        req.flash('error' , 'You donot have the permission to do that');
+        req.flash('error' , 'you donot have the permission to do that');
         return res.redirect('/products');
     }
     next();
 }
 
 const isProductAuthor = async(req,res,next)=>{
-        let {id} = req.params; //product id
-        let product = await Product.findById(id); //entire product
-        if(!product.author.equals(req.user._id)){
-            req.flash('error' , 'You are not the authorised user');
-            return res.redirect('/products');
-        }
-        next();
+//to get the id of thatparticular product
+    let {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product.author);
+    console.log(req.user);
+    if(!product.author.equals(req.user._id)){
+        req.flash('error' , 'you donot have the permission to do that');
+        return res.redirect(`/products/${id}`);
+    }
+    next();
 }
 
 
-
-
-
-module.exports = {isProductAuthor,isSeller,isLoggedIn,validateReview , validateProduct}
+module.exports = {validateProduct ,validateReview , isLoggedIn , isSeller , isProductAuthor} ;
